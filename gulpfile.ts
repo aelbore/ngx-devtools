78b7e3a4e6e960c947da5c26e515b743569d7d8a
aelbore <aelbore.sg@gmail.com>
2018-10-12 17:44:44 +0800

    add publish script

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,6 +4,7 @@ import { cleanAll } from './tools/clean';
 import { packageInit } from './tools/package-init';
 import { unlinkAll } from './tools/unlink-all'; 
 import { build } from './tools/build';
+import { publish } from './tools/publish';
 
 gulp.task('package.init', () => cleanAll().then(() => packageInit()))
 
@@ -12,3 +13,5 @@ gulp.task('build', () => build())
 gulp.task('clean.all', () => cleanAll())
 
 gulp.task('unlink.all', () => unlinkAll())
+
+gulp.task('package.publish', () => publish());
