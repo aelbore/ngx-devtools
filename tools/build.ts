f0196cee2f9387555cb0ce597728a3a421d1546e
aelbore <aelbore.sg@gmail.com>
2018-10-14 22:25:46 +0800

    add func to copy dist

--- tools/build.ts
+++ tools/build.ts
@@ -1,9 +1,13 @@
-import { join, resolve } from 'path';
-import { existsSync } from 'fs';
+import { join, resolve, basename } from 'path';
+import { existsSync, copyFile } from 'fs';
+import { promisify } from 'util';
 
 import { clean } from './clean';
+import { mkdirp } from './mkdirp';
 
 const packages = [ 'common', 'build', 'server', 'task' ];
+
+const copyFileAsync = promisify(copyFile);
 
 async function onBuildAfterHook() {
   const buildAfterPath = join(process.env.APP_ROOT_PATH, 'build-after.js');
@@ -19,12 +23,25 @@ async function onBuildBeforeHook() {
   }
 }
 
+async function copyBuildFile(folder, globFiles) {
+  const files = await globFiles(join(folder, 'dist', '**/*.*'));
+  const distFolder = join('dist', basename(folder));
+  mkdirp(distFolder);
+  return Promise.all(files.map(file => {
+    const destFile = join(distFolder, basename(file));
+    return copyFileAsync(file, destFile);
+  }))
+}
+
 async function build() {
-  const rootFolder = resolve(), appRootPath = process.env.APP_ROOT_PATH;
+  const rootFolder = resolve(), 
+    appRootPath = process.env.APP_ROOT_PATH,
+    commonFolder = join(rootFolder, 'packages/common/src');
 
-  await clean(`.tmp`);
+  await Promise.all([ clean('.tmp'), clean('dist') ]);
 
-  const { ngxBuild } = await import(`${rootFolder}/packages/common/src/build-package`);
+  const { ngxBuild } = await import(`${commonFolder}/build-package`);
+  const { globFiles } = await import(`${commonFolder}/file`);
   
   for (const folder of packages) {
     process.env.APP_ROOT_PATH = join(rootFolder, 'packages', folder);
@@ -46,6 +63,8 @@ async function build() {
     await onBuildBeforeHook();
     await ngxBuild(folder, rollupConfig);
     await onBuildAfterHook();
+
+    await copyBuildFile(process.env.APP_ROOT_PATH, globFiles);
   }
 
   process.env.APP_ROOT_PATH = appRootPath;
